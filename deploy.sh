ece00533e916dc72d6ea7d942eb0c5d65e6f8ea0
fengwei2002 <2480417969@qq.com>
2020-04-16 17:33:26 +0800

    每次提交都会发给你一个邮件，并且每次都会重置fengwei2002.github.io仓库

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ cd docs/.vuepress/dist
 echo 'feng-w.cn' > CNAME
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'yarn run deploy'
 git push -f git@github.com:fengwei2002/fengwei2002.github.io.git master
 cd -

aa8e265db779dc04724347be8c0ccb436218e21a
fengwei2002 <2480417969@qq.com>
2020-05-09 11:54:02 +0800

    更新脚本

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ cd docs/.vuepress/dist
 # echo 'feng-w.cn' > CNAME
 git init
 git add -A
-git commit -m 'yarn run deploy by fengwei'
+git commit -m 'yarn run deploy'
 git push -f git@github.com:fengwei2002/fengwei2002.github.io.git master
 cd -

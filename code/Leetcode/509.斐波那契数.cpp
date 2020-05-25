59e0f2daf68bbc244bbf32903c7b2b6a1fe01478
fengwei2002 <2480417969@qq.com>
2020-05-25 14:07:19 +0800

    leetcode

---
+++ code/Leetcode/509.斐波那契数.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=cpp
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    int fib(int N) {
+        int a = 0;
+        int b = 1;
+        }
+};
+// @lc code=end

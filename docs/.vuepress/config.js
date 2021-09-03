4d9eb0d846909b59947bd195be4e48574034c997
fengwei2002 <konng_0120@qq.com>
2021-09-03 23:50:57 +0800

    update nickname

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -156,7 +156,7 @@ module.exports = {
             },
         ], //navbar导航栏
         personalInfo: {
-            nickname: "konng.vercel.app",
+            nickname: "konng",
             description: "Waiting is a lost art.",
             // description: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=80% height=52 src="//music.163.com/outchain/player?type=2&id=1297742167&auto=0&height=32"></iframe>',
             email: "konng_0120@qq.com",

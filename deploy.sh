43e31c2c86db5bfba76c3846c16949fa9a20478d
fengwei2002 <konng0120@gmail.com>
2022-06-05 19:40:00 +0800

    remove deploy.sh to private

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-set -e
-npm run docs:build
-cd docs/.vuepress/dist
-# echo 'feng-w.cn' > CNAME
-git init
-git add -A
-git commit -m 'yarn run deploy'
-git push -f git@github.com:fengwei2002/fengwei2002.github.io.git master
-cd -
